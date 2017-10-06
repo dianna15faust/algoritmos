34af890ee09f06d0d523821d8a09bb3937efbbca
Aluno <Aluno@172.32.1.30>
2017-10-05 21:56:17 -0300

    - Criação de Função de Retorno - Criação de Classe principal no pacote - Aula 29

---
+++ src/algoritmos/Algoritmos.java
@@ -0,0 +1,27 @@
+
+package algoritmos;
+
+public class Algoritmos {
+public static void quadrado(double n){
+    double r = n*n;
+    System.out.println("");
+    
+}
+public static double metade(double v){
+    double r = v/2;
+    return r;
+    
+}
+
+    public static void main(String[] args) {
+       double raiz;
+       
+       raiz = Math.sqrt(15);
+       
+        System.out.println("A raiz é" + raiz); 
+        
+        quadrado (15);
+        
+        System.out.println("A metade é:"+ metade(15));
+    }
+}
